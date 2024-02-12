4e539c9c98bbf5bd004fc621f195bcb44e29dcd7
xnsei <Passwar@123*>
2024-02-12 17:04:38 +0530

    adding bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,16 @@ export default function Home() {
     return (
         <main className="p-0 m-0">
             <Navbar/>
-            <div className="absolute -z-10 -top-[800px] -left-[800px] h-[1600px] w-[1600px] bg-gradient-radial from-indigo-600 via-black to-black"></div>
+            <div className="absolute -z-10 -top-[800px] -left-[1000px] h-[1600px] w-[2000px] bg-gradient-radial from-indigo-600 via-black to-black"></div>
             <Hero/>
-            <div>
-                <Hero2/>
+            <Hero2/>
+            <div className="py-16" style={{
+                backgroundImage: 'url("/bg-image.png")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                mask: 'linear-gradient(0deg, transparent 0%, white 20%, white 80%, transparent 100%)'
+            }}>
                 <Community/>
             </div>
             <Gallery/>
