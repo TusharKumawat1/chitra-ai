dcc35a7f0841215beb6f361f32fe4effb94d5d29
TheRealKrishna <agarwl.krishna2@gmail.com>
2024-04-06 21:49:27 +0530

    changes!

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,14 +10,11 @@ const config = {
 	],
   prefix: "",
   theme: {
-    screens: {
-      "xs":"425px"
-    },
     container: {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px"
+        "2xl": "1400px",
       },
     },
     extend: {
